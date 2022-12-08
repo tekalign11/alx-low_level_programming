21c4b56be7971b7ad28c4023fca174396aa31adc
tekalign <tekalign1991@gmail.com>
2022-12-08 11:01:58 -0800

    commited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,10 @@
 */
 int main(void)
 {
-
 	printf("size of a char: %i byte(S)\n", size of(char));
 	printf("size of an int: %i byte(S)\n", size of(int));
 	printf("size of a long int: %i byte(S)\n", size of(long int));
 	printf("size of a long long int: %i byte(S)\n", size of(long long));
 	printf("size of a float: %i byte(S)\n", size of(float));
-
 	return (0);
 }
