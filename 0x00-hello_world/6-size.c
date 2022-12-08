2e3bcce765143c5ca0700823f05705e566103815
tekalign <tekalign1991@gmail.com>
2022-12-08 05:55:25 -0800

    my last for today

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - printing out sizes of datas
+ * code by tekalign
+ * Return: 0
+*/
+int main(void)
+{
+	char c
+	int a;
+	long int b;
+	long long int d;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)size of (c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)size of (a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)size of (b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)size of (f));
+	return (0);
+}
+
