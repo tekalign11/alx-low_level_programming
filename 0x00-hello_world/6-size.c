c10260a6eb62c2c882cc97ab425aae0a91fac2f0
tekalign <tekalign1991@gmail.com>
2022-12-08 08:50:37 -0800

    commiting done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)size of (c));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)size of (a));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)size of (b));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)size of (f));
+	printf("size of a char: %lu byte(S)\n", (unsigned long)size of (c));
+	printf("size of an int: %lu byte(S)\n", (unsigned long)size of (a));
+	printf("size of a long int: %lu byte(S)\n", (unsigned long)size of (b));
+	printf("size of a long long int: %lu byte(S)\n", (unsigned long)size of (d));
+	printf("size of a float: %lu byte(S)\n", (unsigned long)size of (f));
 
 	return (0);
 }
