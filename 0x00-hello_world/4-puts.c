335840578123d645284db77b5e78b6e5b2bc5350
tekalign <tekalign1991@gmail.com>
2022-12-08 04:35:45 -0800

    commiting cool

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <conio. h>
+/*
+ * main - print a code using puts
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
