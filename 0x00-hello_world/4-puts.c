32ece7d05f53bc1208b49b6c93960c01a8e42ae6
tekalign <tekalign1991@gmail.com>
2022-12-08 05:06:06 -0800

    commited well

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main -Print a line of code using puts
+/**
+ * main -Print a code using puts
  *
  * Return: 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
