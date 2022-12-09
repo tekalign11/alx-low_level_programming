4216316405cecc85f483a571e48fd54b03bd6ec3
tekalign <tekalign1991@gmail.com>
2022-12-09 13:25:18 -0800

    commited

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,13 @@
 int main(void)
 {
 	int a;
-	for (a = 0; a < 100; a++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar (a + '0');
-		if (a < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(a + '0');
+	if (a < 9)
+		putchar(',');
+	        putchar(' ');
 	}
-	putchar('\n');
+        putchar('\n');
 	return (0);
 }
