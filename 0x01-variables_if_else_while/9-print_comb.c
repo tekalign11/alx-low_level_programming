188b3c7de07b8cfcd659963bd692acd7ba5159b6
tekalign <tekalign1991@gmail.com>
2022-12-09 13:50:38 -0800

    commited

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - Print combination
- *
+ * main - Print all combination of single digits
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	for (n = 48; n = 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
