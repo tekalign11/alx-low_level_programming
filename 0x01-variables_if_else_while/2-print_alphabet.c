534eee25cb78ce466bd4cc487a3d00e3edb6e553
tekalign <tekalign1991@gmail.com>
2022-12-09 09:59:05 -0800

    commiting done now

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in small letter
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar('\n');
+		return (0);
+	}
+}
