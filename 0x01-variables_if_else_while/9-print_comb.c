0bad6745f1a8b166271bdd646c616c98fdaee3ac
tekalign <tekalign1991@gmail.com>
2022-12-09 13:40:49 -0800

    my last

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Print combination
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int a;
-	for (a = 0; a < 10; a++)
+	int n;
+
+	for (n = 48; n = 58; n++)
 	{
-		putchar(a + '0');
-	if (a < 9)
-		putchar(',');
-	        putchar(' ');
-	}
-        putchar('\n');
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
 	return (0);
+	}
 }
