7941d150ad21d18a7aedce07d051ec6b5ff19544
tekalign <tekalign1991@gmail.com>
2022-12-09 10:43:15 -0800

    commiting my task done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	i = 'a';
+	j = 'b';
+	while
+		(i <= 'z') {
+			putchar(i);
+			i++;
+		}
+	while
+		(j <= 'z') {
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}
