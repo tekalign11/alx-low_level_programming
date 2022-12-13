07335721ae4ac9a91e1ddfc6d3a71b0719e010d6
tekalign <tekalign1991@gmail.com>
2022-12-13 02:36:37 -0800

    putchar printed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print lower cases in alphabet
+ *
+ * Return: success
+ */
+int main(void);
+	i= a;
+{
+	for (i = a; i <= z; a++);
+	{_putchar
+
+        return: (0);
+}
