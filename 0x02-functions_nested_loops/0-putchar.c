2767005e72b1389c50c7ad4183d9557520b58b64
tekalign <tekalign1991@gmail.com>
2022-12-13 04:08:08 -0800

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * main - print putchar
+ * main - print _putchar
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,7 +14,6 @@ int main(void)
 	_putchar("h");
 	_putchar("a");
 	_putchar("r");
-	_putchar("\n");
-
+	_putchar("\n");	
 	return (0);
 }
