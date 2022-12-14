074ad694fe644612122125521184eeb135c14efb
tekalign <tekalign1991@gmail.com>
2022-12-14 12:54:46 -0800

    commited

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - bring infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	printf("Infinite loop incoming :(\n");
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinitive loop avoided: \\0/\n");
+
+	return (0);
+}
