c00c6c94ef2d5f39aa04d194cafb5c244f07a5fd
tekalign <tekalign1991@gmail.com>
2022-12-14 04:42:32 -0800

    commiting done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,16 @@ int print_sign(int n)
 	if (n == 0)
 	{
 		return (0);
-		_putchar(0);
+		_putchar('0');
+	}
+	else if (n > 0)
+	{
+		return (1);
+		_putchar('+');
 	}
 	else
-		if (n > 0)
-		{
-			return (1);
-			_putchar(+);
-		}
-		else
-		{
-			return (-1);
-			_putchar(-);
-		}
+	{
+		return (-1);
+		_putchar('-');
+	}
 }
