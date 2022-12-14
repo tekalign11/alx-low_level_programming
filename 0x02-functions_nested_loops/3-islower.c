3585aaee7ca46b7e7051af017f63b6ad95c0d8f8
tekalign <tekalign1991@gmail.com>
2022-12-14 02:57:50 -0800

    declaring variable

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/***
+ * _islower - Checks if the character is lowercase or not
+ * @c: the character
+ * Return: 1 if letter is lower case, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+		return (0);
+}
+
