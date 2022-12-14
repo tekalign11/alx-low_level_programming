1d02a2b73447511bf698a33e9697e5378225ea93
tekalign <tekalign1991@gmail.com>
2022-12-14 04:05:08 -0800

    commited done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
-/***
+/**
  * _islower - Checks lowercase character
  * @c: The character
  * Return: 1 if letter is lower case, 0 if not
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	}
+	else
 		return (0);
-}
 
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character
+ * Return: 1 for alphabetic character or 0 for other else
+ */
+int _isalpha(int c);
+{
+
