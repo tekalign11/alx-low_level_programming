cd09d3490f31250df329dc914c4bbc257a815821
tekalign <tekalign1991@gmail.com>
2022-12-15 09:58:20 -0800

    commited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,7 @@ int main(void)
 
 	for (f = 1; f <= 100; f++)
 	{
-		if (f == 100)
-		{
-			printf("%s", "Buzz ");
-		}
-		else if ((f % 3 == 0) && (f % 5 == 0))
+		if ((f % 3 == 0) && (f % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
