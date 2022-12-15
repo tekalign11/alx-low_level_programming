fd2cf03eff74676e5200caa8cf82cea003e8e99e
tekalign <tekalign1991@gmail.com>
2022-12-15 03:22:56 -0800

    printing numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *
+ *  mul - multiple of two numbers
+ *  @a: first number
+ *  @b: second number
+ *  Return: Always success
+ */
+int mul(int a, int b)
+
+{
+	int a;
+	int b;
+
+	for (a = i, b = j)
+		printf("%d\n", mul(i, j));
+	return (0);
+}
+
