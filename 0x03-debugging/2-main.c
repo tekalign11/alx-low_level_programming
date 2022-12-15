46f43aef980070ee4c6a6a8bccf2411750ff401f
tekalign <tekalign1991@gmail.com>
2022-12-15 14:39:32 -0800

    adding to main h

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints the largest of 3 integers
  * Return: 0
  */
 
 int main(void)
-int largest_number(int a, int b, int c)
-{
-	int a, b, c;
 
+{
+	int a, int b, int c;
 	int largest;
 
 	a = 972;
