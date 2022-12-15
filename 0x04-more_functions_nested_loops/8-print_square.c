233cb196c8730e7ea66e3520647ac50497d9743e
tekalign <tekalign1991@gmail.com>
2022-12-15 05:04:28 -0800

    commited

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @s: size of the square
+ * Return: void
+ */
+void print_square(int s)
+
+{
+	int a;
+	int b;
+
+	for (a = 0; a < s; a++)
+	{
+		for (b = 0; b < s; b++)
+		{
+			_putchar(96);
+		}
+		if (a != s - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
+
