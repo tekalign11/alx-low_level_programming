4db197632c7c5da614ab6e74d9790d76d0657b9e
tekalign <tekalign1991@gmail.com>
2022-12-19 05:02:01 -0800

    commiting putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add numbers
+ * @d: the first number
+ * @f: the second number
+ * Return: Always success
+ */
+int add(int d, int f);
+{
+	int d;
+	int f;
+
+	return (d + f);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * main - prints every minute of the day
+ * Return: Nothing
  */
 
 void jack_bauer(void)
@@ -10,11 +11,15 @@ void jack_bauer(void)
 	int h, m;
 
 	for (h = 0; h <= 23; h++)
+	{
 		for (m = 0; m <= 59; m++)
+		{
 			_putchar((h / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
+		}
+	}
 }
