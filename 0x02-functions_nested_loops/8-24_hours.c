4edd95d4e3700f8ded2a86ff52dc26269aea0653
tekalign <tekalign1991@gmail.com>
2022-12-19 05:31:23 -0800

    commiting hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
