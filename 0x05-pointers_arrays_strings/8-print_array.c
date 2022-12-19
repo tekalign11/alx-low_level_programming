6aba1f6d5b54602db60b81979be939588ee7f7e0
tekalign <tekalign1991@gmail.com>
2022-12-19 13:11:00 -0800

    printing n number of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: an array of intigers
+ * @n: the number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+
+		if (t != n - 1)
+			printf(",");
+			printf(" ");
+	}
+	printf("\n");
+}
