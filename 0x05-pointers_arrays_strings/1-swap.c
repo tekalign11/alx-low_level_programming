848bb6600e20c59e000b28a31afeefea5a84dda7
tekalign <tekalign1991@gmail.com>
2022-12-19 10:03:42 -0800

    adding main

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: the first ineger
+ * @b: the second integers
+ */
+void swap_int(int *a, int *b)
+{
+	int changed;
+
+	changed = *a;
+		*a = *b;
+		changed = *b;
+}
