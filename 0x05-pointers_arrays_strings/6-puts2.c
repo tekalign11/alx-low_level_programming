9a7c903441925edac0349113e08f45cafc967a57
tekalign <tekalign1991@gmail.com>
2022-12-19 12:40:42 -0800

    adding halfs of a number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: half ot the string to be printed
+ */
+void puts_half(char *str)
+{
+	int t;
+
+	t = 0;
+	while (str[t] != '\0')
+	{
+		if (t % 2 == 0)
+			_putchar(str[t]);
+		t++;
+	}
+	_putchar('\n');
+}
