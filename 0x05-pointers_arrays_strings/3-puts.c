c053bdc67893f38e9bbb1706b3a09148685892ae
tekalign <tekalign1991@gmail.com>
2022-12-19 10:47:00 -0800

    commited

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,16 +3,12 @@
 /**
  * _puts - prints a string
  * @str: the string to be printed
- *
- * Return: the string
  */
 void _puts(char *str)
 {
 	int nurse;
 
 	for (nurse = 0; str[nurse] != '\0'; nurse++)
-		_putchar(str[nurse]);
-	_putchar('\n');
-
-	return (0);
+		putchar(str[nurse]);
+	putchar('\n');
 }
