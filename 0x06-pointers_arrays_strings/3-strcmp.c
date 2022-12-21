1d15edf87f09a095de8971eca5cd934b19419ea2
tekalign <tekalign1991@gmail.com>
2022-12-21 12:57:22 -0800

    commited

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,6 +4,7 @@
  *  _strcmp - this function compares two strings
  * @s1: string one
  * @s2: string two
+ * Return: success
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,12 +15,16 @@ int _strcmp(char *s1, char *s2)
 		if (s1[t] == '\0' && s2[t] == '\0')
 			break;
 		else if (s1[t] == '\0')
+		{
 			nur = s2[t];
 			break;
+		}
 		else if (s2[t] == '\0')
+		{
 			nur = s1[t];
 			break;
-		else if (s1[t] != s2[t]
+		}
+		else if (s1[t] != s2[t])
 		{
 			nur = s1[t] - s2[t];
 			break;
