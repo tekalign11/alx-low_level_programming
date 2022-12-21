98a858fe92ce40ca50a0761e896c026764b34f6f
tekalign <tekalign1991@gmail.com>
2022-12-21 13:37:17 -0800

    my six task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: capitalizes a string
+ * Return: success
+ */
+char *cap_string(char *str)
+{
+	int yes = 0;
+
+	while (str[++yes])
+	{
+		while (!(str[yes] >= 'a' && str[yes] <= 'z'))
+			yes++;
+		if (str[yes - 1] == ' ' ||
+				str[yes - 1] == '\t' ||
+				str[yes - 1] == '\n' ||
+				str[yes - 1] == ',' ||
+				str[yes - 1] == ';' ||
+				str[yes - 1] == '.' ||
+				str[yes - 1] == '!' ||
+				str[yes - 1] == '?' ||
+				str[yes - 1] == '"' ||
+				str[yes - 1] == '(' ||
+				str[yes - 1] == ')' ||
+				str[yes - 1] == '{' ||
+				str[yes - 1] == '}')
+			str[yes] -= 32;
+	}
+	return (str);
+}
