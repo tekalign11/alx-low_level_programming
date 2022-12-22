ae5c69f4c79d504dffa9c5248b752b21d268a53a
tekalign <tekalign1991@gmail.com>
2022-12-22 10:20:51 -0800

    commited

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - a function that prints an integer
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int kutir = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		kutir = -kutir;
+	}
+	if ((kutir / 10) > 0)
+		print_number(kutir / 10);
+	_putchar((kutir % 10) + '0');
+}
+
