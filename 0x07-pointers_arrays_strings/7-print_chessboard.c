b755eb24f5fa14927cc5e82158698c4340de4a9d
tekalign11 <tekalign1991@gmail.com>
2023-01-02 04:53:44 -0800

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int teka1, teka2;
+
+	for (teka1 = 0; a[teka1][7]; teka1++)
+	{
+		for (teka2 = 0; teka2 < 8; teka2++)
+		{
+			_putchar(a[teka1][teka2]);
+		_putchar('\n');
+		}
+	}
+}
