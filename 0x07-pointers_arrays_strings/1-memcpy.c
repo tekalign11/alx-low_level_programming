f30c9d6b7d36dbd187ee3a4e7fdd05a574cc8155
tekalign11 <tekalign1991@gmail.com>
2023-01-02 03:42:10 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination
+ * @src: memory area
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0 ; i++, n--)
+		dest[i] = src[i];
+	return (dest);
+}
+
+
