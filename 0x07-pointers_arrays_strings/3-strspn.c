d2b39dcd3c0767678f36e571773b85276f2cbd7c
tekalign11 <tekalign1991@gmail.com>
2023-01-02 04:15:59 -0800

    searching

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,6 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	int index;
 
 	while (*s)
+	{
 		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
@@ -23,6 +24,7 @@ unsigned int _strspn(char *s, char *accept)
 				return (bytes);
 		}
 	s++;
+	}
 	return (bytes);
 }
 
