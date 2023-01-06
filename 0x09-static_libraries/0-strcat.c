72ac9acf165c3ad0a0fffd21c404321b61b6e489
tekalign11 <tekalign1991@gmail.com>
2023-01-06 04:12:40 -0800

    modifying

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - Write a function that concatenates two strings
+ * @dest: one of the strings
+ * @src: the other string
+ * Return:void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int yes = 0, tek = 0;
+
+	while (dest[yes++])
+		tek++;
+	for (yes = 0; src[yes]; yes++)
+		dest[tek++] = src[yes];
+	return (dest);
+}
