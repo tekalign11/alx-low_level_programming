22c2c1ef885e8b267e902ac2d22fce482bc473bf
Tekalign Amera <tekalign1991@gmail.com>
2023-05-18 02:18:26 -0700

    printing using printf and puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts "\"Programming is like building a multilingual puzzle\n"
+#include <stdio.h>
+
+/**
+ * main: Always success
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf "with proper grammar, but the outcome is a piece of art\,"
+#include <stdio.h>
+
+/**
+ * main: prints sa snetence
+ * Return: Always 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
