044b8fe4c9d9a7346e8182158e44f0ed6615de32
Tekalign Amera <tekalign1991@gmail.com>
2023-05-18 03:18:00 -0700

    printing sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - beginning of the function
+ * Description: prints size of data types
+ * Return: Always success
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of an int: %zu byte(s)", sizeof(int));
+	printf("Size of a long int: %zu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+	return (0);
+}
