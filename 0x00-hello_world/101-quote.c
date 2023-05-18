b24e5e0bbee54d1197f6f5fff4a81914e940407d
Tekalign Amera <tekalign1991@gmail.com>
2023-05-18 02:55:08 -0700

    printing to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  * description: print a text on stder but not with puts or printf
- * Return: Always 0(success)
+ * Return: Always 1(success)
  */
 int main(void)
 {
-	fprintf(stderr," and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
