83ff686f48aadc7df291802852749524076e118d
Tekalign Amera <tekalign1991@gmail.com>
2023-05-19 15:10:53 -0700

    print combo of four nums

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,13 +17,16 @@ int main(void)
 			for (k = 0; k < 10; k++)
 				for (l = 1; l < 10; l++)
 				{
-					putchar(48 + i);
-					putchar(48 + j);
-					putchar(32);
-					putchar(48 + k);
-					putchar(48 + l);
-					putchar(44);
-					putchar(32);
+					if (j != l)
+					{
+						putchar(48 + i);
+						putchar(48 + j);
+						putchar(32);
+						putchar(48 + k);
+						putchar(48 + l);
+						putchar(44);
+						putchar(32);
+					}
 				}
 	putchar('\n');
 	return (0);
