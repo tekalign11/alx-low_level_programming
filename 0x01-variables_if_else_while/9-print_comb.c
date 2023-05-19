dc99f48094c32cf7c2729c66517314a69cf9e2e8
Tekalign Amera <tekalign1991@gmail.com>
2023-05-19 13:40:31 -0700

    print nums with coma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	int n;
-	int t;
 
 	for (n = 0; n < 10; n++)
-		for (t = 0; t < 10; t++)
 		{
-			putchar(n + 0);
-			putchar(t + 0);
-			putchar(44);
+			putchar(48 + n);
+			if (n < 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	putchar('\n');
 	return (0);
