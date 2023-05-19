ec90a22f8f9a54458304eb4c518e714a8f620d77
Tekalign Amera <tekalign1991@gmail.com>
2023-05-19 13:59:30 -0700

    print alphabetes in upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,20 @@
 /**
  * main - Entry point
  * description: prints alphabetes in upper and lower using putchar
- * return: 0
+ * Return: Always 0
  */
 int main(void)
 {
-	int n > 65;
+	int n = 65;
 
-	
+	for (n = 97; n < 123; n++)
+	{
 		putchar(n);
-		n++;
-	while (n > 97 & n < 123)
+	}
+	for (n = 65; n < 91; n++)
+	{
 		putchar(n);
-		n++;
+	}
 	putchar('\n');
 	return (0);
 }
