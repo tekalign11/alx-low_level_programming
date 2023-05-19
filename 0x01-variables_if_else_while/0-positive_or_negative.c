c39713aa59ec8f7ddf699c365fde602862cbd708
Tekalign Amera <tekalign1991@gmail.com>
2023-05-19 03:53:42 -0700

    printing if number is pos neg or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -9,9 +10,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if n > 0
+	if (n > 0)
 		printf("%d is positive", n);
-	else if n < 0
+	else if (n < 0)
 		printf("%d is negative", n);
 	else
 		printf("%d is zero", n);
