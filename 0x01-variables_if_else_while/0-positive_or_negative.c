9db8316413a05301b9261bbb43716c8f22a2aa24
Tekalign Amera <tekalign1991@gmail.com>
2023-05-19 04:02:53 -0700

    printing number type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * main - print number type
+ * Description: Prints if number is zero positive or nega
+ * Return: Always success (0)
+*/
+
 int main(void)
 {
 	int n;
@@ -11,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
