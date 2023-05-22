8c4645ed58e7ae0b7feda2dbbcd076dfdd9e48a2
Tekalign Amera <tekalign1991@gmail.com>
2023-05-22 15:48:03 -0700

    print combo of two 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,23 +9,23 @@ int main(void)
 {
 	int m;
 	int n;
-	
+
 	for (m = 0; m < 100; m++)
 		for (n = m + 1; n < 100; n++)
+		{
 			{
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				putchar(32);
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				if (!((m == 98) & (n == 99)))
 				{
-					putchar((m / 10) + 48);
-					putchar((m % 10) + 48);
+					putchar(44);
 					putchar(32);
-					putchar((n / 10) + 48);
-					putchar((n % 10) + 48);
-					if (!((m == 98) & (n == 99)))
-					{
-						putchar(44);
-						putchar(32);
-					}
 				}
 			}
+		}
 	putchar('\n');
 	return (0);
 }
