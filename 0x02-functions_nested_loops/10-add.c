02e1ba514124c79b88bbdba584a8052bdb938f4c
Tekalign Amera <tekalign1991@gmail.com>
2023-05-23 13:57:37 -0700

    print sum of two nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - prints sum of numbers
+ * @t: The first number
+ * @a: The second number
+ * Description: The function prints the sum of two numbers
+ * Return: The sum of the two
+ */
+int add(int t, int a)
+{
+	int sum = t + a;
+
+	return (sum);
+}
