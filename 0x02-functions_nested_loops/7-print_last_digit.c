13f14ad2b59b32ef61c9053c5a53e49d63beba7d
Tekalign Amera <tekalign1991@gmail.com>
2023-05-23 13:08:16 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,14 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @k: The number last digit to be printed
- * Description: print the last digit
+ * @int: The number last digit to be printed
+ * Description: Print the last digit
  * Return: The value of last digit
  */
 int print_last_digit(int k)
 {
-	int n;
+	int n = k % 10;
 
-	n = k % 10;
-	printf("%d", k % 10);
+	printf("%d", n);
 	return (n);
 }
