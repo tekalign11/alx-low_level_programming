cb4a255b9c1b0c0943da5c9e5a8fba78e4f775ff
Tekalign Amera <tekalign1991@gmail.com>
2023-05-23 04:08:57 -0700

    prints string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * this function prints a string
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A project to solve advanced problems to be solvd using loop and loop control statemnts
