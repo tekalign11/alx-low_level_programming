b88154c2be7ca4c28d09796f1b4443f209060eb1
Tekalign Amera <tekalign1991@gmail.com>
2023-05-23 14:06:45 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: The beginning of number to be checked
+ * Description: This function prints from n to 98
+ * Return: Always success (0)
+ */
+void print_to_98(int n)
+{
+	for (; n > 98; n--)
+	{
+		printf("%d, \n", n);
+	}
+	for (; n < 98; n ++)
+	{
+		printf("%d, \n", n);
+	}
+}
