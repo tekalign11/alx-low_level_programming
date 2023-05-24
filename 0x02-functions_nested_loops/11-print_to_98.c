12005bb401cc918f9848ad91510236ed65e80e6f
Tekalign Amera <tekalign1991@gmail.com>
2023-05-24 15:52:40 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,15 @@ void print_to_98(int n)
 	int i;
 
 	for (i = n; i > 97; i--)
-		printf("%d, ", i);
-	printf("\n");
+	{
+		printf("%d", i);
+		if (i != 98)
+			printf(", \n");
+	}
 	for (i = n; i < 99; i++)
+	{
 		printf("%d, ", i);
-	printf("\n");
+		if (i != 98)
+			printf(", \n");
+	}
 }
