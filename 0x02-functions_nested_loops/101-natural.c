a2d01b0db78cbe85899e1ad6623472b055c03630
Tekalign Amera <tekalign1991@gmail.com>
2023-05-24 09:02:07 -0700

    add multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,15 +11,15 @@ int main(void)
 	int sum1 = 0;
 	int sum2 = 0;
 
+	for (t = 3; t < 1024; t += 3)
+	{
+		sum1 =  sum1 + t;
+	}
+	for (a = 5; a < 1024; a += 5)
+	{
+		sum2 = sum2 + a;
+	}
 
-	for (t = 3; t < 1024; t =+ 3)
-	{
-		sum1 =  t + sum1;
-	}
-	for (a = 5; a < 1024; a =+ 5)
-	{
-		sum2 = a + sum2;
-	}
 	printf("%d\n", sum1 + sum2);
 	return (0);
-}	
+}
