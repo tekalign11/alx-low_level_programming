db46a76e26e93d6b5010bc017f5d2ea90c5db3f3
Tekalign Amera <tekalign1991@gmail.com>
2023-05-24 09:21:24 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-/**main - Entry point
+/**
+ * main - Entry point
  * Description: Prints the first 50 fibonaci numbers
  * Return: Always success
  */
 int main(void)
 {
 	int n, m;
+	int sum = 0;
 
-	for (n = 1; n < 51, n++)
-		for (m = 0; m =(( n - 1) + (n - 2)); m++)
-
+	for (n = 1; n < 51; n++)
+	{
+		sum = sum + n;
+		printf("%d, ", sum);
+	}
+	return (0);
+}
