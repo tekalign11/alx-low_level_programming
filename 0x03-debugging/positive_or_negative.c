27350dd948a279bcc070bf51b06e01056e0fac86
Tekalign Amera <tekalign1991@gmail.com>
2023-05-25 12:23:12 -0700

    check pos or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
 
 /**
@@ -9,11 +11,14 @@ int main(void)
 {
 	int i;
 
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
         if (i > 0)
                 printf("%d is positive\n", i);
-        else if (i < 0)
-                printf("%d is negative\n", i);
-        else
+        else if (i == 0)
                 printf("%d is zero\n", i);
+        else
+                printf("%d is negative\n", i);
         return (0);
 }
