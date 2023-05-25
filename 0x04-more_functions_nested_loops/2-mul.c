25a5e81502e8e90fd6752ec603decd09e9129472
Tekalign Amera <tekalign1991@gmail.com>
2023-05-25 05:04:17 -0700

    printes multiple of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: the first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always success (0)
+ */
+int mul(int a, int b)
+{
+	int multiple = a * b;
+	
+	return (multiple);
+}
