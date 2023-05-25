e054e88821c2366c6bcd91e7ae635bec5453a106
Tekalign Amera <tekalign1991@gmail.com>
2023-05-25 14:40:04 -0700

    print to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints to 14 10 times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int t, k;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (k = 0; k < 15; k++)
+		{
+
+			_putchar(k + '0');
+		}
+		_putchar(k + '0');
+	}
+	_putchar('\n');
+}
