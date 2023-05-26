9ed264b170745f6c7fc42d171a4b93652e244351
Tekalign Amera <tekalign1991@gmail.com>
2023-05-26 06:33:26 -0700

    print largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,33 +12,38 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 	
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
-	else if (a == b && b == c)
+	else if (a == b && a == c)
 	{
 		largest = a;
 	}
-	else if (a < b && b == c)
+	else if (a == b && a < c)
+	{
+		largest = c;
+	}
+	else if (a == b && a > c)
+	{
+		largest = a;
+	}
+	else if (b == c && b < a)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
 	{
 		largest = b;
 	}
-	else if (a == b && b > c)
-	{
-		largest = a;
-	}
-	else if (a == c && a > b)
-	{
-		largest = a;
-	}
 	return (largest);
 }
+	
