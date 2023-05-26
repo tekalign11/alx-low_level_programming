9972cad65948b924bd5ec6bacf7a365df6a90858
Tekalign Amera <tekalign1991@gmail.com>
2023-05-26 15:08:28 -0700

    print to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ void more_numbers(void)
 	{
 		for (k = 0; k < 15; k++)
 		{
-			_putchar(k / 10 + '0');
+			_putchar((k / 10) + '0');
 			if (k > 9)
 			{
-				_putchar(k % 10 + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
