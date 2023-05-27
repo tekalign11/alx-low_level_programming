29be42a10ad9c844571c27c8d8a0568b6f28bd50
Tekalign Amera <tekalign1991@gmail.com>
2023-05-27 11:16:12 -0700

    draw square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,20 @@ void print_square(int size)
 	int t;
 	int k = 1;
 
-	while (k <= size)
+	if (size <= 0)
 	{
-		for (t = 1; t <= size; t++)
-		{
-			if (size <= 0)
-				_putchar('\n');
-			else
-				_putchar('#');
-		}
-		k++;
 		_putchar('\n');
+	}
+	else
+	{
+		while (k <= size)
+		{
+			for (t = 1; t <= size; t++)
+			{
+				_putchar('#');
+			}
+			k++;
+			_putchar('\n');
+		}
 	}
 }
