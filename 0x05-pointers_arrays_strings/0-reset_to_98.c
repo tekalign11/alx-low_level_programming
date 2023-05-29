0782af37eb3ab5f1ed0c925c9f6bb4501f889a29
Tekalign Amera <tekalign1991@gmail.com>
2023-05-29 10:35:28 -0700

    assign new value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - resents pointer value to 98
- *
- * @*n: The pointer pointing to an int
- *
+ * Assigns new value to the integer pointed to
+ * @n: The pointer pointing to an int
+ * Assigns new value to the pointer points to
  */
 void reset_to_98(int *n)
 {
