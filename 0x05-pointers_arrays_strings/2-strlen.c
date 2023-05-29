2f57d06ef87bb79f9309e99b88f591eb51ed737d
Tekalign Amera <tekalign1991@gmail.com>
2023-05-29 11:05:19 -0700

    return string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * This function returns the length of a string
+ *
+ * @s: The string whose length to be returned
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+
+	return (strlen(s));
+}
