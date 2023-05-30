d20a3b5cfafa30f771b4b6661a67e9c3ddb3435e
Tekalign Amera <tekalign1991@gmail.com>
2023-05-30 07:56:14 -0700

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: the string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int t = strlen(s);
+
+	for (i = t - 1; i >= 0; i--)
+	{
+		printf("%c", i);
+	}
+}
