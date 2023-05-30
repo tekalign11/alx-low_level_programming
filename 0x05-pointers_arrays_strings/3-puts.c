67f5a6ebe1618becf8cbc6b23b236ff84176e44b
Tekalign Amera <tekalign1991@gmail.com>
2023-05-30 06:01:26 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * This function prints a string to the standard output
+ *
+ * @str: The string to be printe
+ *
+ */
+void _puts(char *str)
+{
+	char t[]= "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts(t);
+}
