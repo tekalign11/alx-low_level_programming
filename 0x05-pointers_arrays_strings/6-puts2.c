1d7fdc4c4e580914203ff174430abbeaf92e9ba1
Tekalign Amera <tekalign1991@gmail.com>
2023-05-30 16:13:14 -0700

    print every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,16 @@
  */
 void puts2(char *str)
 {
-	int i = strlen(str);
+	int i = 0;
 	int j;
 
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 	for (j = 0; j < i; j += 2)
 	{
-		printf("%c", str[j]);
+		_putchar(str[j]);
 	}
 	printf("\n");
 }
