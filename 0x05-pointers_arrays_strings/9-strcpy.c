b06f2dc7efc9d7e1502423ba4ede0a6a4bcfa57a
Tekalign Amera <tekalign1991@gmail.com>
2023-05-31 14:19:56 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ *
+ * @dest: pointed to
+ * @src: String pointed to by
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+
+	src = dest;
+	return (src);
+}
