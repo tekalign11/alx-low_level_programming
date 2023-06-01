235728032228d538215f6f4ff7c572717ab5bd8b
Tekalign Amera <tekalign1991@gmail.com>
2023-06-01 00:57:32 -0700

    print n elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,12 +7,11 @@
  */
 void print_array(int *a, int n)
 {
-	int array;
-	a = &array;
 	int i;
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d, ", a[i]);
 	}
+	printf("\n");
 }
