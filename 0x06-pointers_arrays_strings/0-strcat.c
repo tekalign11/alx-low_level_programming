5df679284ed70158e8d6154d095d8810755a0a20
Tekalign Amera <tekalign1991@gmail.com>
2023-06-04 02:18:37 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,17 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
+	int j = 0;
 
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
-		*dest = *dest + src[i];
 		i++;
 	}
-	*dest = '\0';
-
-	return (0);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
