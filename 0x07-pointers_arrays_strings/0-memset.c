2d2ec70b4f6a30c6ca2e369199b23cb792c56d6a
Tekalign Amera <tekalign1991@gmail.com>
2023-06-05 10:41:55 -0700

    fills memory with n bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ *
+ * @s: Pointer pointing to a string
+ *
+ * @b: The constant
+ *
+ * Return: Pointer to the memory area of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
