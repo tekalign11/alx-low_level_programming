1170b164b492393ebf8e95c799b1475a9fb63b9c
Tekalign Amera <tekalign1991@gmail.com>
2023-06-05 10:54:15 -0700

    fills memory with a constant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int t = (int)n;
-	int i = 0;
+	unsigned int i = 0;
 
-	while (i < t)
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
