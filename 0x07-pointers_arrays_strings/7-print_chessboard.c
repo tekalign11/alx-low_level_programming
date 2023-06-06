e4fc9ae39b7783b3e041654d05c7097d2b9b5212
Tekalign Amera <tekalign1991@gmail.com>
2023-06-06 15:41:30 -0700

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chess board
+ *
+ * @a: the array
+ * @8: The column
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	int s;
+
+	for(i = 0; i < s; i++)
+		for (j = 0; j < 8; j++)
+			a[s][8];
+}
