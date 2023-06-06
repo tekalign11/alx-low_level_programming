fc6948f70cc865858be9aa2e2eea5508da12330e
Tekalign Amera <tekalign1991@gmail.com>
2023-06-06 12:50:13 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int tek = 0;
+	int j = 0;
 
-
-	return (0);
+	while (src[tek] != '\0')
+	{
+		tek++;
+	}
+	while (j < n && j <= tek)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
 }
