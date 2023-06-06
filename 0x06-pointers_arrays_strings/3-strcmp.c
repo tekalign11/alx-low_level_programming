b0a2e882ce144558cd7fa3c3f92085170fa3e93e
Tekalign Amera <tekalign1991@gmail.com>
2023-06-06 13:22:02 -0700

    compares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+	int r = 0;
 
-
-	return (0);
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		r = r + (s1[i] - s2[i]);
+		i++;
+	}
+	return (r);
 }
