f941ef0150fa5bf00230fe52630f826304014b1d
Tekalign Amera <tekalign1991@gmail.com>
2023-06-06 16:10:22 -0700

    print matrix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,17 @@
  * print_chessboard - prints chess board
  *
  * @a: the array
- * @8: The column
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	int s;
 
-	for(i = 0; i < s; i++)
+	for (i = 0; i < 8; i++)
+	{
 		for (j = 0; j < 8; j++)
-			a[s][8];
+		{
+			_putchar(a[i][j]);
+		}
+	_putchar('\n');
+	}
 }
