a2fbe3b41f2a67432e194d2256d393bb7fdfcffe
Tekalign Amera <tekalign1991@gmail.com>
2023-06-07 09:31:25 -0700

    prints string using recursive function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	if (s[i] == '\0')
+		_putchar('\n');
+	else
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		_puts_recursion(s + 1 );
 	}
-	_putchar('\n');
 }
