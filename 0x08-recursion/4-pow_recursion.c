aae22fbfbd8f9116f654202058c75c97f1514a32
Tekalign Amera <tekalign1991@gmail.com>
2023-06-07 11:48:30 -0700

    returns power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,3 +8,16 @@
  */
 
 int _pow_recursion(int x, int y)
+{
+	int tek;
+
+	if ( y < 0)
+		tek = -1;
+	else if (y == 0)
+		tek = 1;
+	else if (y == 1)
+		tek = x;
+	else
+		tek = x * _pow_recursion(x, y - 1);
+	return (tek);
+}
