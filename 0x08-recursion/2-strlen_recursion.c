f0c167f1469891ba90be9d737736bc5d61defd5d
Tekalign Amera <tekalign1991@gmail.com>
2023-06-08 09:26:20 -0700

    prints length odf the string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,12 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
-		return (i);
-	else
+	if (s[i] != '\0')	
 	{
 		i = 1 + _strlen_recursion(s + 1);
 	}
+	else
+		return (i);
+
 	return (i);
 }
