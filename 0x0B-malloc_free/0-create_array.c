e769d574477eb22e3e7bbc9edc7762429fbef344
Tekalign Amera <tekalign1991@gmail.com>
2023-06-14 00:29:50 -0700

    creates array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	ptr = malloc(size);
 
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 	while (i < size)
 	{
 		ptr[i] = c;
