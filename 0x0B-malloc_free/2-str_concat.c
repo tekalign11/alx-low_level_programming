36be02259ca4ee5d35b40cb96535fae35747ab3a
Tekalign Amera <tekalign1991@gmail.com>
2023-06-14 05:38:51 -0700

    allocates memory to a concatenated strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,6 +26,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (concptr == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
 	while (t < i)
 	{
 		concptr[t] = s1[t];
