3d09dc6b2586915f1f468ea1dd4aca51b8b0ab16
Tekalign Amera <tekalign1991@gmail.com>
2023-06-14 04:39:01 -0700

    concat strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,4 +8,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0;
+	int t = 0;
+	int k = 0;
 
+	while (s1[i] != 0)
+	{
+		i++;
+	}
+	while (s2[k] != 0)
+	{
+		k++;
+	}
+	s1 = malloc(i + k + 1);
+
+	if (s1 == NULL)
+		return (NULL);
+	while (t <= (k))
+	{
+		s1[i + 1] = s2[t];
+		t++;
+		i++;
+	}
+	return (s1);
+	free(s1);
+}
