9d98c1e680e201258422bfc796246bbfa890cbfe
Tekalign Amera <tekalign1991@gmail.com>
2023-06-14 10:17:40 -0700

    dynamically allocate 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,4 +8,24 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i = 0;
+	int j = 0;
+	int **mygrid;
 
+	mygrid = malloc(4 * width * height + 4);
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	while (i < width)
+	{
+		while (j < height)
+		{
+			mygrid[i][j] = i + j;
+			i++;
+			j++;
+		}
+	}
+	return (mygrid);
+}
