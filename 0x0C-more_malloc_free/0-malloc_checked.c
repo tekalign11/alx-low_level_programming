794e16c6b07c2c49927cf946b3c46348f2bf27d5
Tekalign Amera <tekalign1991@gmail.com>
2023-06-15 11:48:21 -0700

    allocate memory with exit ststaus

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,10 +3,35 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: The numbers of integers to be stored
+ * Return: Returns nothing
  */
 void *malloc_checked(unsigned int b)
 {
+	char *chr;
+	int *inr;
+	float *flt;
+	double *dbl;
 
+	chr = malloc(sizeof(char) * b);
 
+	if (chr == NULL)
+		exit(98);
+	return (chr);
+	inr = malloc(sizeof(int) * b);
 
+	if (inr == NULL)
+		exit(98);
+	return (inr);
+
+	flt = malloc(sizeof(float) * b);
+
+	if (flt == NULL)
+		exit(98);
+	return (flt);
+
+	dbl = malloc(b * sizeof(double));
+
+	if (dbl == NULL)
+		exit(98);
+	return (dbl);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
