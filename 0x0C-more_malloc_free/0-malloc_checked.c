e5d66cecf10a82f17f8536a976416e29454d4387
Tekalign Amera <tekalign1991@gmail.com>
2023-06-15 10:18:23 -0700

    allocates memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: The numbers of integers to be stored
+ */
+void *malloc_checked(unsigned int b)
+{
+
+
+
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The size of the string
+ * Return: The concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+
+}
+
