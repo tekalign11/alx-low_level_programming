f84da3493af874eefa092fd2a7a7fe08a3c16626
Tekalign Amera <tekalign1991@gmail.com>
2023-06-16 03:42:07 -0700

    calloc using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements of the array
+ * @size: size of the array
+ * Return: The pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a = 0;
+	void *narray;
+
+	narray = malloc(nmemb * size);
+
+	if (narray == NULL)
+		return (NULL);
+	if (nmemb == 0 || nmemb == 0)
+		return (NULL);
+	for (a = 0; a < nmemb; a++)
+		*((char *)narray + a * size) =  0;
+	return (narray);
+}
