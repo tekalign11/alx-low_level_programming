f004d1bca4cab40786fdb7b54d5791d4d4da851d
Tekalign Amera <tekalign1991@gmail.com>
2023-06-16 03:46:16 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	narray = malloc(nmemb * size);
+	narray = malloc(nmemb * size + 1);
 
 	if (narray == NULL)
 		return (NULL);
