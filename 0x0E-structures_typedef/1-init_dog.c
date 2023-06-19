87b2d1e6c368d0b06a220318c2e2083b35f57ba2
Tekalign Amera <tekalign1991@gmail.com>
2023-06-19 14:40:52 -0700

    initializes a struct variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 
 /**
  * init_dog - initializes the struct dog
@@ -11,20 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = "Shasho";
-	(*d).age = 7;
-	(*d).owner = "Abebe";
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
