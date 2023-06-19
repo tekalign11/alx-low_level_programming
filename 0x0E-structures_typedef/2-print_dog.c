c92c1f41a322398dc64ae9cccad0fe0ecaef70e2
Tekalign Amera <tekalign1991@gmail.com>
2023-06-19 15:07:22 -0700

    prints a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints the struct dog
+ * @d: pointer to the struct
+ */
+
+void print_dog(struct dog *d)
+{
+
+	printf("Name: %s\n", (*d).name != NULL ? (*d).name : "(nil)");
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner != NULL ? (*d).owner : "(nil)");
+}
