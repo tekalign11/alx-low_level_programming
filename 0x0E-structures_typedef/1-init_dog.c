147c7e5ea2e7d174dbcbe29a051d56192a92a068
Tekalign Amera <tekalign1991@gmail.com>
2023-06-19 14:08:24 -0700

    initializes struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -31,3 +31,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	*d = other_dog;
 }
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
