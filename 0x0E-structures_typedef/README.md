ca466efd5229082e036f619b69f09623f7240a48
Tekalign Amera <tekalign1991@gmail.com>
2023-06-19 13:19:04 -0700

    creates a struct of dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains files on c structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGSTRUCT_H
+#define DOGSTRUCT_H
+/**
+ * struct dog - creates a new struct variable dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
