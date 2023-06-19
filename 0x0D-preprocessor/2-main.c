213f5a09511a3d88fd8a045543219c83fa409abd
Tekalign Amera <tekalign1991@gmail.com>
2023-06-19 09:53:53 -0700

    print file name using macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main: Program entry point
+ * Description: Prints file name using predifined macro
+ * Return: Always success
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
