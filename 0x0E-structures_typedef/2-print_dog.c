8eb4bf8ed86074a8d40683efe3bd76c763ce2c32
Tekalign Amera <tekalign1991@gmail.com>
2023-06-20 14:03:05 -0700

    print  a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,10 @@
 void print_dog(struct dog *d)
 {
 
-	printf("Name: %s\n", (*d).name != NULL ? (*d).name : "(nil)");
-	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner != NULL ? (*d).owner : "(nil)");
+	if(d != NULL)
+	{
+		printf("Name: %s\n", (*d).name != NULL ? (*d).name : "(nil)");
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: %s\n", (*d).owner != NULL ? (*d).owner : "(nil)");
+	}
 }
