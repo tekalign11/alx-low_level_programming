784eb5ded54162625086a713fe89961ec95c85c3
Tekalign Amera <tekalign1991@gmail.com>
2023-06-21 12:57:50 -0700

    prints using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - prints name
+ * @name: The nname to be printed
+ * @f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
