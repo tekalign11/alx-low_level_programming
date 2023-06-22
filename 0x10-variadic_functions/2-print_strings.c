37f74b318456e9a12a7789c455c2610ad90ad80d
Tekalign Amera <tekalign1991@gmail.com>
2023-06-22 15:15:51 -0700

    prints string using variadic

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: the separator string
+ * @n: the size of args
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int t;
+	char *m;
+	va_list teka;
+
+	va_start(teka, n);
+
+	if (separator == NULL)
+		return;
+	for (t = 0; t < n; t++)
+	{
+		m = va_arg(teka, char *);
+		if (m == NULL)
+			printf("nil");
+		else
+			printf("%s", m);
+		if (t < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
