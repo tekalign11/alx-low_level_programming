93526f4d2a82da3c8ba384a1aa6f49548cee5cc7
Tekalign Amera <tekalign1991@gmail.com>
2023-06-22 14:59:57 -0700

    prints args with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL || n <= 0)
+	if (separator == NULL)
 		return;
 	for (t = 0; t < n; t++)
 	{
