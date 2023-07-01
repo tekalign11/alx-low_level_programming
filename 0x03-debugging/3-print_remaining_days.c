90958b923d38e46bcccc276da9757598eeea8ef8
Tekalign Amera <tekalign1991@gmail.com>
2023-07-01 15:51:12 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	int y = year % 4;
 
-	if (((year % 4 == 0 && !(year % 100 == 0)) || ((year % 4 == 0) && (year % 400 == 0))))
+	if (((y == 0 && !(year % 100 == 0)) || ((y == 0) && (year % 400 == 0))))
 	{
-		if (month >= 2 && day >= 59)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
@@ -24,11 +25,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month >= 2 && day == 60)
+		if (month == 2 && day == 60)
 		{
-			day++;
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
 	}
 }
