c833e948d53d1e8cea6289d5c0b5431ce2309ec8
Tekalign Amera <tekalign1991@gmail.com>
2023-07-01 14:04:55 -0700

    print fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int n1 = 0, n2 = 1;
+	int n1 = 1, n2 = 2;
 	int i, n3;
 
 	printf("%d, %d, ", n1, n2);
-	for (i = 2; i < 51; i++)
+	for (i = 3; i < 51; i++)
 	{
-		printf("%d, ", n3);
-		n2 = n1;
-		n3 = n2;
 		n3 = n1 + n2;
+		printf("%d, ", n3);
+		n1 = n2;
+		n2 = n3;
 	}
 	return (0);
 }
