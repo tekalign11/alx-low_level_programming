9d9a509cd66a8a32e3e5951870b434510a743d02
Tekalign Amera <tekalign1991@gmail.com>
2023-07-03 14:11:38 -0700

    print number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,13 +11,10 @@ size_t list_len(const list_t *h)
 
 	if (h == NULL)
 		return (0);
-	else
+	while (h != NULL)
 	{
-		while (h != NULL)
-		{
-			t++;
-			h = h->next;
-		}
+		t++;
+		h = h->next;
 	}
 	return (t);
 }
