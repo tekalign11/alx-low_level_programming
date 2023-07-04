550e1fb9d280ae02d0627ddd0d0f44c8a355ec44
Tekalign Amera <tekalign1991@gmail.com>
2023-07-04 14:53:15 -0700

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	newnode = (listint_t *)malloc(sizeof(listint_t));
-
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->next = *head;
 	*head = newnode;
