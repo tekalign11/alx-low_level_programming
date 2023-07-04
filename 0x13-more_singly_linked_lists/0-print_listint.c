a81d68f3aca6b059af60844312413bf23aecfe6f
Tekalign Amera <tekalign1991@gmail.com>
2023-07-04 13:29:25 -0700

    prints elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of a list
+ * @h: ointer to the ist
+ * Return: the numebr of elements of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int t = 0;
+	const listint_t* ptr;
+
+	ptr = h;
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		t++;
+		ptr = ptr->next;
+	}
+	return (t);
+}
