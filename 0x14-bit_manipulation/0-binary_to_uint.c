20d84b2502a133e8e16e0e372f823baabb5f79e3
Tekalign Amera <tekalign1991@gmail.com>
2023-07-07 07:05:18 -0700

    convert decimal to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int z = 0;
+	unsigned int z = 0;
 
 	if (b == NULL)
 	{
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: the number to be converted to binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int m;
+
+	if (n == 0)
+		_putchar('0');
+	while (n > 0)
+	{
+		m = ((n >> 1));
+		if ((m * 2) == n)
+			_putchar('0');
+		else
+			_putchar('1');
+		n = m;
+	}
+}
