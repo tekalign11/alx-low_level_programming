a860b58f2c795571be7f012d2136cffdab7a712d
Tekalign Amera <tekalign1991@gmail.com>
2023-07-07 14:22:13 -0700

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (n == NULL)
 		return (-1);
 	if (index > sizeof(unsigned int) * 8)
-		return (0);
+		return (-1);
 	m = 1 << index;
 	*n = *n | m;
 	return (1);
