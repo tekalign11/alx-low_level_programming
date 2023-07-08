12180a276946a6df877f24f0f7f11a90c6aad28a
Tekalign Amera <tekalign1991@gmail.com>
2023-07-08 13:54:01 -0700

    done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -29,7 +29,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	}
 	if (index >= k)
 		return (0);
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	while (k > 0)
 	{
