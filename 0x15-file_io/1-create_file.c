380dd506012eff5e00da8da2d5b34e4f1d69a6bd
Tekalign Amera <tekalign1991@gmail.com>
2023-07-10 14:38:38 -0700

    append text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,6 @@ int create_file(const char *filename, char *text_content)
 			t_len++;
 		}
 	}
-
 	descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (descriptor < 0)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			t_len++;
 		}
 	}
-	app = write(descriptor, text_content, t_len);
+	app = write(descriptor, text_content, t_len + 1);
 	if (app < 0)
 	{
 		close(descriptor);
