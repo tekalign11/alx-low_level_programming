a762be7e0908dca88e0c491aa87a507f0ec29847
Tekalign Amera <tekalign1991@gmail.com>
2023-07-10 14:28:34 -0700

    appends text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t app;
 	int t_len = 0;
 
+	if (filename == NULL)
+		return (-1);
 	descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (descriptor < 0)
 	{
