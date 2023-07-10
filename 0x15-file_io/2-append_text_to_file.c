816f09aeda5e616f61774d589df2c6244faca0ed
Tekalign Amera <tekalign1991@gmail.com>
2023-07-10 14:19:36 -0700

    appends text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: pointer to the name of the file
+ * @text_content: pointer to text to be added
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int descriptor;
+	ssize_t app;
+	int t_len;
+
+	descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (descriptor < 0)
+	{
+		close(descriptor);
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(descriptor);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[t_len])
+		{
+			t_len++;
+		}
+	}
+	app = write(descriptor, text_content, t_len);
+	if (app < 0)
+	{
+		close(descriptor);
+		return (-1);
+	}
+	close(descriptor);
+	return (1);
+}
