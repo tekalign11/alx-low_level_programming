7c1a6b674fb459abff355fa2aa7d9444d075002d
Tekalign Amera <tekalign1991@gmail.com>
2023-07-23 02:19:14 -0700

    copy contents of a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	file_to = argv[2];
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", file_from, file_to);
 		exit(97);
 	}
 	copy_content(file_from, file_to);
